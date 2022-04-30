e85a981ece7da7325905fce14921ea31118522d2
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-04-30 23:51:06 +0300

    Practicing data gatherer from web site

---
+++ 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/ChitankaDataGatherer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DemoExercise
+{
+    public class ChitankaDataGatherer
+    {
+        public List<RawProperty> GatherData(int maxId)
+        {
+            var properties = new List<RawProperty>();
+
+            string regex = @"[А-Яа-я]\—";
+
+            for (int i = 1; i <= maxId; i++)
+            {
+                var html = GetHtml(i).Result;
+
+                // Indexes of start / end
+                var start = html.IndexOf("<title>");
+                var end = html.IndexOf("— Моята библиотека");
+                var result = html
+                    .Substring(start + 7, end - start - 8)
+                    .Replace("\n", "")
+                    .Replace("\r", "")
+                    .Replace("\t", "");
+
+                string author = String.Empty;
+                string title = String.Empty;
+
+                Match match = Regex.Match(result, regex);
+
+                if (match.Success)
+                {
+                    var regexValue = match.Value;
+                    var index = result.IndexOf(regexValue) + 2;
+                    author = result.Substring(0, index - 1).Trim();
+                    title = result.Substring(index).Trim();
+
+                }
+                else
+                {
+                    var lastDash = result.LastIndexOf(" — ");
+
+                    if (lastDash < 0)
+                    {
+                        author = result.Trim();
+                    }
+                    else
+                    {
+                        author = result.Substring(0, lastDash).Trim();
+                        title = result.Substring(lastDash + 2).Trim();
+                    }
+                }
+                properties.Add(new RawProperty(author, title));
+            }
+
+            return properties;
+        }
+        public static async Task<string> GetHtml(int index)
+        {
+            HttpClient httpClient = new HttpClient();
+            var url = $"https://chitanka.info/book/{index}";
+            var bookInfo = await httpClient.GetStringAsync(url);
+            return bookInfo;
+        }
+    }
+
+    public class RawProperty
+    {
+        public RawProperty(string author, string title)
+        {
+            Author = author;
+            Title = title;
+        }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace DemoExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var dataGatherer = new ChitankaDataGatherer();
+
+            var properties = dataGatherer.GatherData(10);
+
+            var serialize = JsonConvert.SerializeObject(properties, Formatting.Indented);
+
+            Console.WriteLine(serialize);
+        }
+    }
+}
