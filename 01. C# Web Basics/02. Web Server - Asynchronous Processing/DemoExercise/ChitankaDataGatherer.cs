87c9ec3e217d5018b0cc634a0865f9133d3a74bb
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-05-01 01:36:44 +0300

    Add AngleSharp package

--- 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/ChitankaDataGatherer.cs
+++ 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/ChitankaDataGatherer.cs
@@ -5,11 +5,79 @@ using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Parser;
 
 namespace DemoExercise
 {
     public class ChitankaDataGatherer
     {
+        private List<RawProperty> rawProperties;
+
+        public ChitankaDataGatherer()
+        {
+            rawProperties = new List<RawProperty>();
+        }
+
+        public List<RawProperty> GetRawProperties()
+        {
+            return rawProperties;
+        }
+
+        public async void GatherDataWithAngleSharp(int maxId)
+        {
+            var parser = new HtmlParser();
+            var config = Configuration.Default;
+            var context = BrowsingContext.New(config);
+
+            string regex = @"[А-Яа-я]\—";
+
+            for (int i = 1; i <= maxId; i++)
+            {
+                var document = await context
+                    .OpenAsync(req => req.Content(GetHtml(i).Result));
+
+                var element = document.All
+                    .Where(x => x.LocalName == "title")
+                    .FirstOrDefault();
+                
+
+                var result = element.TextContent.Replace("\n", String.Empty)
+                        .Replace("\r", String.Empty)
+                        .Replace("\t", String.Empty)
+                        .Replace("— Моята библиотека", String.Empty);
+
+                string author = String.Empty;
+                string title = String.Empty;
+
+                Match match = Regex.Match(result, regex);
+
+                if (match.Success)
+                {
+                    var regexValue = match.Value;
+                    var index = result.IndexOf(regexValue) + 2;
+                    author = result.Substring(0, index - 1).Trim();
+                    title = result.Substring(index).Trim();
+
+                }
+                else
+                {
+                    var lastDash = result.LastIndexOf(" — ");
+
+                    if (lastDash < 0)
+                    {
+                        author = result.Trim();
+                    }
+                    else
+                    {
+                        author = result.Substring(0, lastDash).Trim();
+                        title = result.Substring(lastDash + 2).Trim();
+                    }
+                }
+                rawProperties.Add(new RawProperty(author, title));
+            }
+        }
+
         public List<RawProperty> GatherData(int maxId)
         {
             var properties = new List<RawProperty>();
@@ -23,6 +91,7 @@ namespace DemoExercise
                 // Indexes of start / end
                 var start = html.IndexOf("<title>");
                 var end = html.IndexOf("— Моята библиотека");
+
                 var result = html
                     .Substring(start + 7, end - start - 8)
                     .Replace("\n", "")
--- 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/Program.cs
+++ 01. C# Web Basics/02. Web Server - Asynchronous Processing/DemoExercise/Program.cs
@@ -10,15 +10,21 @@ namespace DemoExercise
     {
         static void Main(string[] args)
         {
+            //Source: https://chitanka.info/
+
             Console.OutputEncoding = Encoding.UTF8;
 
             var dataGatherer = new ChitankaDataGatherer();
 
-            var properties = dataGatherer.GatherData(10);
+            // 05/01/2022 - 10762 books
+            var properties = dataGatherer.GatherData(100);
+
+            dataGatherer.GatherDataWithAngleSharp(100);
+            var listOfDataFromAngle = dataGatherer.GetRawProperties();
 
             var serialize = JsonConvert.SerializeObject(properties, Formatting.Indented);
 
-            Console.WriteLine(serialize);
+            File.WriteAllText(@"../../../chitanka.bg-raw-data.json", serialize);
         }
     }
 }
