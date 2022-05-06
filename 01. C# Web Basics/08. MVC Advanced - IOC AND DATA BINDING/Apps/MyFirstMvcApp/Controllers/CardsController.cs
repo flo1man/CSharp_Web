7f7ae0fabf906030e40a65b60280c7aacc2779d1
Stivan Kostadinov <stivan.kostadinov@gmail.com>
2022-05-06 19:16:49 +0300

    Implement login and register pages

---
+++ 01. C# Web Basics/08. MVC Advanced - IOC AND DATA BINDING/Apps/MyFirstMvcApp/Controllers/CardsController.cs
@@ -0,0 +1,88 @@
+﻿using BattleCards.Data;
+using BattleCards.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleCards.Controllers
+{
+    public class CardsController : Controller
+    {
+        // GET /cards/add
+        public HttpResponse Add()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            return this.View();
+        }
+
+        [HttpPost("/Cards/Add")]
+        public HttpResponse DoAdd()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var dbContext = new ApplicationDbContext();
+
+            if (this.Request.FormData["name"].Length < 5)
+            {
+                return this.Error("Name should be at least 5 characters long.");
+            }
+
+            dbContext.Cards.Add(new Card
+            {
+                Attack = int.Parse(this.Request.FormData["attack"]),
+                Health = int.Parse(this.Request.FormData["health"]),
+                Description = this.Request.FormData["description"],
+                Name = this.Request.FormData["name"],
+                ImageUrl = this.Request.FormData["image"],
+                Keyword = this.Request.FormData["keyword"],
+            });
+            dbContext.SaveChanges();
+
+            return this.Redirect("/Cards/All");
+        }
+
+        // /cards/all
+        public HttpResponse All()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            var db = new ApplicationDbContext();
+            var cardsViewModel = db.Cards.Select(x => new CardViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Attack = x.Attack,
+                Health = x.Health,
+                ImageUrl = x.ImageUrl,
+                Type = x.Keyword,
+            }).ToList();
+
+            return this.View(cardsViewModel);
+        }
+
+        // /cards/collection
+        public HttpResponse Collection()
+        {
+            if (!this.IsUserSignedIn())
+            {
+                return this.Redirect("/Users/Login");
+            }
+
+            return this.View();
+        }
+    }
+}
---
+++ 01. C# Web Basics/08. MVC Advanced - IOC AND DATA BINDING/Apps/MyFirstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using BattleCards.ViewModels;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BattleCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.Redirect("/Cards/All");
+            }
+
+            return this.View();
+        }
+    }
+}
